7969c22a4b4779b13ef1b8fa918a39cc5170af0e
Gowtham R <gowtham080802@gmail.com>
2024-08-21 14:18:47 +0530

    Initial commit

---
+++ src/main/java/org/medx/elixrlabs/ElixrLabsApplication.java
@@ -0,0 +1,13 @@
+package org.medx.elixrlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElixrLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElixrLabsApplication.class, args);
+	}
+
+}
