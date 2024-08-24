1bf6a3cd063e24089491104b4cf19ecb6ffaede2
deolinjaffens <deomuja@gmail.com>
2024-08-24 15:04:55 +0530

    minor changes

--- src/main/java/org/medx/elixrlabs/service/impl/SampleCollectorServiceImpl.java
+++ src/main/java/org/medx/elixrlabs/service/impl/SampleCollectorServiceImpl.java
@@ -1,20 +1,20 @@
-//package org.medx.elixrlabs.service.impl;
-//
-//import org.medx.elixrlabs.dto.UserDto;
-//import org.medx.elixrlabs.mapper.UserMapper;
-//import org.medx.elixrlabs.model.SampleCollector;
-//import org.medx.elixrlabs.model.User;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class SampleCollectorServiceImpl {
-//
-//    public UserDto createSampleCollector(UserDto userDto) {
-//        User user = UserMapper.toUser(userDto);
-//        SampleCollector sampleCollector = SampleCollector.builder()
-//                .user(user)
-//                .isVerified(false)
-//                .build();
-//    }
-//
-//}
+package org.medx.elixrlabs.service.impl;
+
+import org.medx.elixrlabs.dto.UserDto;
+import org.medx.elixrlabs.mapper.UserMapper;
+import org.medx.elixrlabs.model.SampleCollector;
+import org.medx.elixrlabs.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SampleCollectorServiceImpl {
+
+    public UserDto createSampleCollector(UserDto userDto) {
+        User user = UserMapper.toUser(userDto);
+        SampleCollector sampleCollector = SampleCollector.builder()
+                .user(user)
+                .isVerified(false)
+                .build();
+    }
+
+}
