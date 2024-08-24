65a5c928c6f2e442b3c6b8989ef9f5111dacc88d
Sabarinathan <sabarinathan6802@gmail.com>
2024-08-24 10:28:42 +0530

    Changed in user dto

--- src/main/java/org/medx/elixrlabs/dto/UserDto.java
+++ src/main/java/org/medx/elixrlabs/dto/UserDto.java
@@ -9,11 +9,21 @@ import org.medx.elixrlabs.util.GenderEnum;
 
 import java.time.LocalDate;
 
+/**
+ * <p>
+ * This class is responsible for transferring user data as Data Transfer Object (DTO)
+ * between layers.
+ * This class is used to encapsulate the data related to an user and
+ * transfer it between the client and the server. DTOs are typically used to
+ * decouple the internal representation of data (employee entity class) from
+ * the API, providing a simpler and more controlled way to expose data.
+ * </p>
+ */
 @Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class RegisterAndLoginUserDto {
+public class UserDto {
     private String email;
     private String password;
     private LocalDate dob;
