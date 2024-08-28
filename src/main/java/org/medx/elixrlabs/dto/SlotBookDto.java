dfda57e132624591f58d42f5007331b97d3335c7
Gowtham R <gowtham080802@gmail.com>
2024-08-28 11:01:00 +0530

    Changes in security and added validation for slot dto

--- src/main/java/org/medx/elixrlabs/dto/SlotBookDto.java
+++ src/main/java/org/medx/elixrlabs/dto/SlotBookDto.java
@@ -1,8 +1,12 @@
 package org.medx.elixrlabs.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.medx.elixrlabs.annotation.TimeSlot;
 import org.medx.elixrlabs.util.LocationEnum;
 import org.medx.elixrlabs.util.TestCollectionPlaceEnum;
 
@@ -11,8 +15,13 @@ import java.time.LocalDate;
 @Data
 @Builder
 public class SlotBookDto {
+    @NotNull
     private LocationEnum location;
+    @NotNull
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
+    @TimeSlot
     private String timeSlot;
+    @NotNull
     private TestCollectionPlaceEnum testCollectionPlace;
 }
