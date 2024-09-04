390cc90e0c6a5d4b21ef3e28f56c2c32e41da6f2
deolinjaffens <deomuja@gmail.com>
2024-09-04 11:51:56 +0530

    Added test for controllers

--- src/test/java/org/medx/elixrlabs/controller/PatientControllerTest.java
+++ src/test/java/org/medx/elixrlabs/controller/PatientControllerTest.java
@@ -4,20 +4,29 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.medx.elixrlabs.dto.*;
-import org.medx.elixrlabs.model.LabTest;
 import org.medx.elixrlabs.service.AppointmentSlotService;
 import org.medx.elixrlabs.service.CartService;
 import org.medx.elixrlabs.service.PatientService;
 import org.medx.elixrlabs.util.GenderEnum;
 import org.medx.elixrlabs.util.LocationEnum;
 import org.medx.elixrlabs.util.TestCollectionPlaceEnum;
+import org.medx.elixrlabs.util.TestStatusEnum;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class PatientControllerTest {
@@ -36,12 +45,16 @@ public class PatientControllerTest {
 
     private UserDto userDto;
     private ResponsePatientDto responsePatientDto;
-    private RequestSlotBookDto slotBookDto;
+    private RequestSlotBookDto requestSlotBookDto;
+    private SlotBookDto slotBookDto;
     private TestResultDto testResultDto;
     private ResponseTestInCartDto test;
     private List<ResponseTestInCartDto> tests;
     private CartDto cartDto;
     private ResponseCartDto responseCartDto;
+    private ResponseOrderDto order;
+    private List<ResponseOrderDto> orders;
+    private OrderSuccessDto orderDto;
 
     @BeforeEach
     void setUp() {
@@ -50,27 +63,33 @@ public class PatientControllerTest {
                 .place(LocationEnum.VELACHERY)
                 .password("user@123")
                 .phoneNumber("1234567890")
-                .dateOfBirth(LocalDate.parse("1999-02-25"))
+                .dateOfBirth(LocalDate.of(1999, 2, 25))
                 .gender(GenderEnum.M)
                 .build();
         responsePatientDto = ResponsePatientDto.builder()
                 .id(1L)
-                .dateOfBirth(LocalDate.parse("1999-02-25"))
+                .dateOfBirth(LocalDate.of(1999, 2, 25))
                 .email("user@gmail.com")
                 .gender(GenderEnum.M)
                 .phoneNumber("1234567890")
                 .place(LocationEnum.VELACHERY)
                 .build();
-        slotBookDto = RequestSlotBookDto.builder()
+        requestSlotBookDto = RequestSlotBookDto.builder()
+                .date(LocalDate.parse("2024-09-01"))
+                .location(LocationEnum.VELACHERY)
+                .testCollectionPlace(TestCollectionPlaceEnum.HOME)
+                .build();
+        slotBookDto = SlotBookDto.builder()
+                .timeSlot("7PM")
                 .date(LocalDate.parse("2024-09-01"))
                 .location(LocationEnum.VELACHERY)
                 .testCollectionPlace(TestCollectionPlaceEnum.HOME)
                 .build();
         testResultDto = TestResultDto.builder()
-                .generatedAt(LocalDateTime.parse("2024-09-01"))
+                .generatedAt(LocalDateTime.now())
                 .id(1L)
                 .email("user@gmail.com")
-                .orderDate(LocalDate.parse("2024-08-29"))
+                .orderDate(LocalDate.of(2024, 8, 29))
                 .result(List.of("Normal"))
                 .ageAndGender("24 M")
                 .build();
@@ -91,5 +110,93 @@ public class PatientControllerTest {
                 .price(200.00)
                 .testPackage(null)
                 .build();
+        order = ResponseOrderDto.builder()
+                .testStatus(TestStatusEnum.IN_PROGRESS)
+                .tests(tests)
+                .id(1L)
+                .build();
+        orders = List.of(order, order);
+        orderDto = OrderSuccessDto.builder()
+                .dateTime(testResultDto.getGeneratedAt())
+                .id(1L)
+                .build();
+    }
+
+    @Test
+    void testCreateOrUpdatePatient() {
+        when(patientService.createOrUpdatePatient(userDto)).thenReturn(responsePatientDto);
+        ResponseEntity<ResponsePatientDto> result = patientController.createOrUpdatePatient(userDto);
+        assertEquals(responsePatientDto, result.getBody());
+        assertEquals(HttpStatus.CREATED, result.getStatusCode());
+    }
+
+    @Test
+    void testGetAvailableSlots() {
+        when(appointmentSlotService.getAvailableSlots(any(RequestSlotBookDto.class))).thenReturn(Set.of("1AM", "2AM"));
+        ResponseEntity<Set<String>> result = patientController.getAvailableSlots(requestSlotBookDto);
+        assertEquals(Set.of("1AM", "2AM"), result.getBody());
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void testBookSlot() {
+        when(appointmentSlotService.bookSlot(any(SlotBookDto.class))).thenReturn(orderDto);
+        ResponseEntity<OrderSuccessDto> result = patientController.bookSlot(slotBookDto);
+        assertEquals(orderDto, result.getBody());
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void testGetOrders() {
+        when(patientService.getOrders()).thenReturn(orders);
+        ResponseEntity<List<ResponseOrderDto>> result = patientController.getOrders();
+        assertEquals(orders, result.getBody());
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void testGetTestReport() {
+        when(patientService.getTestReport(anyLong())).thenReturn(testResultDto);
+        ResponseEntity<TestResultDto> result = patientController.getTestReport(order.getId());
+        assertEquals(testResultDto, result.getBody());
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void testUpdatePatient() {
+        when(patientService.createOrUpdatePatient(userDto)).thenReturn(responsePatientDto);
+        ResponseEntity<ResponsePatientDto> result = patientController.updatePatient(userDto);
+        assertEquals(responsePatientDto, result.getBody());
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void testDeletePatient() {
+        doNothing().when(patientService).deletePatient();
+        ResponseEntity<HttpStatus.Series> result = patientController.deletePatient();
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void testAddTestsOrPackageToCart() {
+        when(cartService.addTestsOrPackagesToCart(any(CartDto.class))).thenReturn(responseCartDto);
+        ResponseEntity<ResponseCartDto> result = patientController.addTestOrPackagesToCart(cartDto);
+        assertEquals(responseCartDto, result.getBody());
+        assertEquals(HttpStatus.ACCEPTED, result.getStatusCode());
+    }
+
+    @Test
+    void testGetCart() {
+        when(cartService.getCartByPatient()).thenReturn(responseCartDto);
+        ResponseEntity<ResponseCartDto> result = patientController.getCart();
+        assertEquals(responseCartDto, result.getBody());
+        assertEquals(HttpStatus.OK, result.getStatusCode());
+    }
+
+    @Test
+    void testRemoveTestsOrPackagesFromCart() {
+        doNothing().when(cartService).deleteCart();
+        ResponseEntity<HttpStatus.Series> result = patientController.removeTestsOrPackageFromCart();
+        assertEquals(HttpStatus.OK, result.getStatusCode());
     }
 }
