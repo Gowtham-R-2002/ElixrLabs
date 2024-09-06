0c417c21f690d57c9a3e23235598c55552204855
Sabarinathan <sabarinathan6802@gmail.com>
2024-09-06 11:01:00 +0530

    Fix : Custom annotation

--- src/main/java/org/medx/elixrlabs/annotation/LabTestsValidator.java
+++ src/main/java/org/medx/elixrlabs/annotation/LabTestsValidator.java
@@ -10,16 +10,21 @@ public class LabTestsValidator implements ConstraintValidator<LabTests, List<Lon
 
     @Override
     public boolean isValid(List<Long> labTests, ConstraintValidatorContext context) {
+        context.disableDefaultConstraintViolation();
         if (null == labTests) {
-            context.buildConstraintViolationWithTemplate("LabTest ID's must not be null");
+            context.buildConstraintViolationWithTemplate("LabTest ID's must not be null")
+                    .addConstraintViolation();
             return false;
         }
         if (labTests.isEmpty()) {
-            context.buildConstraintViolationWithTemplate("Test Package must contain at least one Lab Test");
+            context.buildConstraintViolationWithTemplate("Test Package must contain at least one Lab Test")
+                    .addConstraintViolation();
             return false;
         }
         if (new HashSet<>(labTests).size() != labTests.size()) {
-            context.buildConstraintViolationWithTemplate("Lab Test ID's must be unique");
+            context.buildConstraintViolationWithTemplate("Lab Test ID's must be unique")
+                    .addConstraintViolation();
+            return false;
         }
         return true;
     }
